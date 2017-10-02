34ee89e1781e28f616bd55daefddb7c66497959f
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-01 21:06:41 -0500

    set up app.js

--- pizza-shop/src/App.js
+++ pizza-shop/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Layout from .'components/layout'
+import store, { history } from './data/store'
+
+const App = () => {
+  <div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Layout />
+      </ConnectedRouter>
+    </Provider>
+  </div>
 }
 
 export default App;
