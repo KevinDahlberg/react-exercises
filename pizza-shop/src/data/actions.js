e4ae17a099fbb63947b0fdad52fa7f72fa550dbe
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-10-01 20:54:13 -0500

    set up actions and reducers

---
+++ pizza-shop/src/data/actions.js
@@ -0,0 +1,36 @@
+import fetch from 'isomorphic-fetch'
+
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+export const CLEAR_CART = 'CLEAR_CART'
+export const CONFIRM_CART = 'CONFIRM_CART'
+export const CONFIRM_ORDER = 'CONFIRM_ORDER'
+
+export function addToCart(product){
+  return {
+    type: ADD_TO_CART,
+    product
+  }
+}
+
+export function removeFromCart(product) {
+  return {
+    type: REMOVE_FROM_CART,
+    product
+  }
+}
+
+export function clearCart (productArray) {
+  type: CLEAR_CART
+  productArray
+}
+
+export function confirmCart(productArray) {
+  type: CONFIRM_CART,
+  productArray
+}
+
+export function confirmOrder(productArray) {
+  type: CONFIRM_ORDER,
+  productArray
+}
---
+++ pizza-shop/src/data/reducers.js
@@ -0,0 +1,37 @@
+import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+import {
+  ADD_TO_CART
+  REMOVE_FROM_CART
+  CONFIRM_CART
+  CONFIRM_ORDER
+} from './actions'
+
+function cartReducer (state = {}, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return { ...state, cart: [...state.cart, action.product]}
+    case REMOVE_FROM_CART:
+      return { ...state, cart: state.cart.filter(product => action.product !== product)}
+    case CLEAR_CART:
+      return { ...state, cart: []}
+    case CONFIRM_CART:
+      return state
+    default:
+      return state
+  }
+}
+
+function orderReducer (state = {}, action) {
+  if (action.type === 'CONFIRM_ORDER'){
+    return { ...state, cart: []}
+  } else {
+    return state
+  }
+}
+
+const rootReducer = combineReducers({
+  cartReducer,
+  orderReducer,
+  routing: routerReducer
+})
